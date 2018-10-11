7eed4a4529801829f89421f2c65d90db7a3dda63
togacoder <bike.dio@gmail.com>
2018-10-11 13:30:01 +0900

    ifelse

--- app.py
+++ app.py
@@ -6,21 +6,39 @@ app = Flask(__name__)
 @app.route('/echo', methods=['POST'])
 def echo():
     message = request.json.get("queryResult").get("parameters").get("message")
-    response = {
-        "payload": {
-            "google": {
-                "expectUserResponse": True,
-                "richResponse": {
-                    "items": [
-                        {
-                            "simpleResponse": {
-                                "textToSpeech": message 
+    if message == "hello":
+        response = {
+            "payload": {
+                "google": {
+                    "expectUserResponse": True,
+                    "richResponse": {
+                        "items": [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": message 
+                                }
                             }
-                        }
-                    ]
+                        ]
+                    }
                 }
             }
         }
-    }
+    else:
+        response = {
+            "payload": {
+                "google": {
+                    "expectUserResponse": True,
+                    "richResponse": {
+                        "items": [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": 'else' 
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
 
     return json.dumps(response)
