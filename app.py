b95af6f14b43d95df9bf12b99257e40faefc6152
togacoder <bike.dio@gmail.com>
2018-10-11 16:39:00 +0900

    back

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, request
 import json
-import requests
 
 app = Flask(__name__)
-
-def post_GAS(data):
-    url = 'https://script.google.com/macros/s/AKfycbyWIu6E1aH_NGNJfNSCCdqCdbwXopzmQxvO91nlcGugASqWrcc/exe://script.google.com/macros/s/AKfycbyWIu6E1aH_NGNJfNSCCdqCdbwXopzmQxvO91nlcGugASqWrcc/exec'
-    response = requests.post(url, data)
 
 @app.route('/echo', methods=['POST'])
 def echo():
@@ -45,6 +40,5 @@ def echo():
                 }
             }
         }
-    post_GAS(response)
 
     return json.dumps(response)
