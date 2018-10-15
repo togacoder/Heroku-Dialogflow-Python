22c063093bad4291b1e560b773e60c3c5b3273ba
togacoder <bike.dio@gmail.com>
2018-10-15 13:15:22 +0900

    json

--- app.py
+++ app.py
@@ -6,48 +6,41 @@ app = Flask(__name__)
 
 @app.route('/echo', methods=['POST'])
 def echo():
+    # queryResult.queryText : 認識文字列
+    # parameters : 変数
     message = request.json.get("queryResult").get("parameters").get("message");
     data = request.json
     post_SpreadSheets(data)
-
-    if message == "hello":
-        response = {
-            "payload": {
-                "google": {
-                    "expectUserResponse": True,
-                    "richResponse": {
-                        "items": [
-                            {
-                                "simpleResponse": {
-                                    "textToSpeech": "Hello World." 
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-    else:
-        response = {
-            "payload": {
-                "google": {
-                    "expectUserResponse": True,
-                    "richResponse": {
-                        "items": [
-                            {
-                                "simpleResponse": {
-                                    "textToSpeech": "Hello Heroku." 
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        }
     
+    if message == 'hello':
+        message = 'Hello, World!!' 
+    else:
+        message = 'hello, Heroku.'
+    
+    response = make_json(message)
+
     post_SpreadSheets(response)
 
     return json.dumps(response) 
+
+def make_json(message):
+    response = {
+            "payload": {
+                "google": {
+                    "expectUserResponse": True,
+                    "richResponse": {
+                        "items": [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": message 
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    return response
 
 def post_SpreadSheets(data):
     url = 'https://script.google.com/macros/s/AKfycbyWIu6E1aH_NGNJfNSCCdqCdbwXopzmQxvO91nlcGugASqWrcc/exec'
