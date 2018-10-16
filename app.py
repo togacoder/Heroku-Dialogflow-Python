2a588994037cd0e629da1e579eff813a3d51137a
togacoder <bike.dio@gmail.com>
2018-10-16 17:20:19 +0900

    post

--- app.py
+++ app.py
@@ -6,24 +6,24 @@ app = Flask(__name__)
 
 @app.route('/echo', methods=['POST'])
 def echo():
-    # queryResult.queryText : 認識文字列
-    # parameters : 変数
     message = request.json.get("queryResult").get("parameters").get("message");
     post_SpreadSheets(message)
-    
+
+    # 会話の制御
     if message == 'hello':
         message = 'Hello, World!!' 
     else:
         message = 'hello, Heroku.'
     
-    response = make_json(message)
-
+    response = res_json(message)
     post_SpreadSheets(message)
 
+    # Dialogflow に返す
     return json.dumps(response) 
 
 
-def make_json(message):
+# Dialogflow に返すjsonを作成
+def res_json(message):
     response = {
             "payload": {
                 "google": {
@@ -43,6 +43,7 @@ def make_json(message):
     return response
 
 
+# Google SpreadSheets にPOSTする
 def post_SpreadSheets(data):
     url = 'https://script.google.com/macros/s/AKfycbyWIu6E1aH_NGNJfNSCCdqCdbwXopzmQxvO91nlcGugASqWrcc/exec'
     res = requests.post(url, json.dumps(data))    
--- jsonTest.js
+++
@@ -1,5 +0,0 @@
-var e = '{contents={"payload": {"google": {"expectUserResponse": true, "richResponse": {"items": [{"simpleResponse": {"textToSpeech": "Hello, World!!"}}]}}}}, length=138, name=postData, type=}';
-
-e = JSON.parse(e);
-
-console.log(typeof(e));
