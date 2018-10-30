45fe2c5a8cb16cc3fa6c59e810359bdbb8cff334
togacoder <bike.dio@gmail.com>
2018-10-30 13:49:05 +0900

    encode

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 from flask import Flask, request
 import json
 import requests
@@ -14,8 +16,8 @@ def echo():
     # Intent Name
     displayName = request.json.get("queryResult").get("intent").get("displayName")
 
-    post_SpreadSheets(displayName)
-    post_SpreadSheets(message)
+    post_SpreadSheets(displayName.encode('utf-8'))
+    post_SpreadSheets(message.encode('utf-8'))
 
     # 会話の制御
     # message = 'レスポンス'
@@ -30,39 +32,39 @@ def echo():
     elif displayName == 'Exit':
         message = 'Outing Agentを終了します。'
     
-    response = res_json(message)
-    post_SpreadSheets(message)
+    res = res_json(message)
+    post_SpreadSheets(message.encode('utf-8'))
 
     # Dialogflow に返す
-    return json.dumps(response) 
+    return json.dumps(res) 
 
 
 # Dialogflow に返すjsonを作成
 def res_json(message):
-    response = {
-            "payload": {
-                "google": {
-                    "expectUserResponse": True,
-                    "richResponse": {
-                        "items": [
-                            {
-                                "simpleResponse": {
-                                    "textToSpeech": message 
-                                }
+    res = {
+        "payload": {
+            "google": {
+                "expectUserResponse": True,
+                "richResponse": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": message 
                             }
-                        ]
-                    }
+                        }
+                    ]
                 }
             }
         }
-    return response
+    }
+    return res
 
 
 # Google SpreadSheets にPOSTする
 def post_SpreadSheets(data):
     # Outing URL
     url = 'https://script.google.com/macros/s/AKfycbze93rXUHBp1vcHF5GQLxUqbgPrQeNjGwCOBCFYleBaZNiJm3_C/exec'
-    res = requests.post(url, json.dumps(data))    
+    res = requests.post(url, json.dumps(data)) 
 
 
 if __name__ == '__main__':
