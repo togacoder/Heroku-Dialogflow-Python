90c06a1ae298ff8d441e61a1c80f4967ba239a2a
togacoder <bike.dio@gmail.com>
2018-10-30 12:51:07 +0900

    Outing 10

--- post_request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'https://script.google.com/macros/s/AKfycbyWIu6E1aH_NGNJfNSCCdqCdbwXopzmQxvO91nlcGugASqWrcc/exec'
-response = requests.post(url, data = { 'queryText': 'data1', 'fulfillmentText': 'data2' })
-print(response.status_code)
-print(response.text)
